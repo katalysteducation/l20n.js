aae4030ab25974d4f85a26778d558470a66072eb
Tomek Stach <tomasz.stach@gmail.com>
2016-10-14 13:27:01 +0200

    Merge with master of the l20n

--- src/runtime/gecko/util.js
+++ src/runtime/gecko/util.js
@@ -1,6 +1,3 @@
-import { contexts } from '../../lib/dom/base';
-import { valueFromContext } from '../../lib/format';
-
 export {
   documentReady as HTMLDocumentReady, getResourceLinks
 } from '../web/util';
@@ -16,40 +13,4 @@ export function XULDocumentReady() {
       resolve();
     });
   });
-}
-
-export function createGetValue(bundles) {
-  return function (id, args) {
-    const ctx = contexts.get(bundles[0]);
-    const [value] = valueFromContext(ctx, id, args);
-    return value;
-  };
-}
-
-// create nsIObserver's observe method bound to a LocalizationObserver obs
-export function createObserve(obs) {
-  return function observe(subject, topic, data) {
-    switch (topic) {
-      case 'language-registry-update': {
-        this.requestLanguages();
-        return obs.translateRoots(this);
-      }
-      case 'language-registry-incremental': {
-        const { resId, lang, messages } = JSON.parse(data);
-        return this.interactive.then(bundles => {
-          const bundle = bundles[0];
-          if (resId in bundle.resources && bundle.locale === lang) {
-            // just overwrite any existing messages in the first bundle
-            const ctx = contexts.get(bundles[0]);
-            ctx.addMessages(messages);
-            obs.translateRoots(this);
-          }
-          return bundles;
-        });
-      }
-      default: {
-        throw new Error(`Unknown topic: ${topic}`);
-      }
-    }
-  }
 }
